edbe7b7e3cb1ae28396de3e70abd6fa8d1cbcc78
Giuseppe Campanelli <suprememilanfan@gmail.com>
2020-10-02 10:00:40 -0400

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    public GameObject MainMenu;
+    public GameObject About;
+
+    void Start() {
+        MainMenuButton();
+    }
+
+    public void PlayNowButton() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
+    }
+
+    public void AboutButton() {
+        MainMenu.SetActive(false);
+        About.SetActive(true);
+    }
+
+    public void MainMenuButton() {
+        MainMenu.SetActive(true);
+        About.SetActive(false);
+    }
+
+    public void QuitButton() {
+        Application.Quit();
+    }
+}
