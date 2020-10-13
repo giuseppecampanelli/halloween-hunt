77b1598d44f132d426ca5ef1f98edcc4a881a586
Giuseppe Campanelli <suprememilanfan@gmail.com>
2020-10-13 18:04:21 -0400

    Added score text

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour
 {
+    Text scoreObject;
+
+    private int score = 0;
+
     void Start()
     {
         Cursor.visible = false;
+
+        scoreObject = GameObject.Find("Score").GetComponent<Text>();
+        scoreObject.text = score.ToString();
     }
 
-    void Update ()
+    void Update()
     {
         transform.position = Input.mousePosition;
-     }
+    }
 }
