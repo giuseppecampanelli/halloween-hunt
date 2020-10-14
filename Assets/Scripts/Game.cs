37342deb4f3f164db2d9179163f894481c1bdc94
Giuseppe Campanelli <suprememilanfan@gmail.com>
2020-10-14 15:25:56 -0400

    Added timer and font

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public float timeStart = 60;
+    public int level = 1;
+    public int score = 0;
+    public int enemiesRemaining = 10;
+
+    public Text timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        timer.text = Mathf.Round(timeStart).ToString();
+
+        if (timeStart <= 0) {
+            // game over
+        }
+    }
+}
