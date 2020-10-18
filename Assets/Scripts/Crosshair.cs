8155cdd40fe0dc7689298b0c8ecd7056e283a95f
Giuseppe Campanelli <suprememilanfan@gmail.com>
2020-10-18 15:22:44 -0400

    Added continuous firing mode

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -11,6 +11,10 @@ public class Crosshair : MonoBehaviour
     private float pumpkinTime = 3;
     private bool pumpkinVisible;
 
+    public float fireTime = 3;
+    public float fireRate = 0.25f;
+    private bool firing;
+
     void Start()
     {
         Cursor.visible = false;
@@ -20,44 +24,61 @@ public class Crosshair : MonoBehaviour
     {
         transform.position = Input.mousePosition;
 
-        if (Input.GetMouseButtonDown(0)) {
-            fireSound.Play(0);
+        if (!firing) {
+            CancelInvoke("FireShot");
+        }
 
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D[] hits = Physics2D.RaycastAll(pos, new Vector2(0, 0), 0.01f);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !firing) {
+            firing = true;
 
-            if (hits.Length > 0) {
-                int enemiesHit = 0;
+            StartCoroutine(DisableContinuousFire());
+        }
 
-                for (int i = 0; i < hits.Length && enemiesHit <= 2; i++) {
-                    if (hits[i].collider.tag == "Ghost") {
-                        enemiesHit++;
-                        Destroy(hits[i].collider.gameObject);
-                        Game.scoreValue += 3;
-                        Game.enemiesRemaining--;
-                    } else if (hits[i].collider.tag == "Witch") {
-                        enemiesHit++;
-                        Destroy(hits[i].collider.gameObject);
-                        Game.scoreValue += 10;
-                    }
-                }
-
-                if (enemiesHit == 2)
-                    Game.scoreValue = 5;
-            } else {
-                if (!pumpkinVisible) {
-                    pumpkin.SetActive(true);
-                    pumpkinVisible = true;
-                    StartCoroutine(LateCall());
-                }
-                
-                Game.scoreValue -= 1;
-            }
+        if (Input.GetMouseButtonDown(0) && firing) {
+            InvokeRepeating("FireShot", 0, fireRate);
+        } else if (Input.GetMouseButtonDown(0)) {
+            FireShot();
         }
     }
 
-    IEnumerator LateCall()
-     {
+    void FireShot()
+    {
+        fireSound.Play(0);
+
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(pos, new Vector2(0, 0), 0.01f);
+
+        if (hits.Length > 0) {
+            int enemiesHit = 0;
+
+            for (int i = 0; i < hits.Length && enemiesHit <= 2; i++) {
+                if (hits[i].collider.tag == "Ghost") {
+                    enemiesHit++;
+                    Destroy(hits[i].collider.gameObject);
+                    Game.scoreValue += 3;
+                    Game.enemiesRemaining--;
+                } else if (hits[i].collider.tag == "Witch") {
+                    enemiesHit++;
+                    Destroy(hits[i].collider.gameObject);
+                    Game.scoreValue += 10;
+                }
+            }
+
+            if (enemiesHit == 2)
+                Game.scoreValue += 5;
+        } else {
+            if (!pumpkinVisible) {
+                pumpkin.SetActive(true);
+                pumpkinVisible = true;
+                StartCoroutine(PumpkinDisappear());
+            }
+            
+            Game.scoreValue -= 1;
+        }
+    }
+
+    IEnumerator PumpkinDisappear()
+    {
         if (pumpkinVisible) {
             yield return new WaitForSeconds(pumpkinTime);
  
@@ -66,5 +87,11 @@ public class Crosshair : MonoBehaviour
                 pumpkin.SetActive(false);
             }
         }
-     }
+    }
+
+    IEnumerator DisableContinuousFire()
+    {
+        yield return new WaitForSeconds(fireTime);
+        firing = false;
+    }
 }
